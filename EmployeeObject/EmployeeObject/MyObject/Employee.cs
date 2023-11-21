56369bc70cd3715a0bf9fcc23dee77bdf0f72312
Harshith VH <vhharshith77@gmail.com>
2023-11-21 17:39:45 +0530

    Add files via upload

---
+++ EmployeeObject/EmployeeObject/MyObject/Employee.cs
@@ -0,0 +1,123 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeObject.MyObject
+{
+    // access modifiers
+    // private, public, internal, protected and internal protected
+    // Classes under namespace can either be public/internal
+    // if made public it will be availabe in the current package also visible to packages outside
+    // if made internal -> will be visible only within the current package (EmployeeObject)
+    internal class Employee
+    {
+        //internal int EmployeeId;
+        // if access modifier is not mentioned then default is private
+
+        // use ctor shortcut for default constructor
+        public Employee()
+        {   
+            DOB = DateTime.Now;
+            EmployeeCounter++;
+        }
+
+        // this() will call the default Employee() constructor and then executes the next three lines
+        public Employee(int eid, string name) :this()
+        {
+            EmployeeId = eid;
+            Name = name;
+            Salary = 10000;
+        }
+
+        // use propfull shortcut(TAB)
+        private int _intEmployeeId;
+        public int EmployeeId
+        {
+            get 
+            { 
+                  return _intEmployeeId;
+            }
+            set 
+            { 
+                if(value > 0) { 
+                    _intEmployeeId = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid Id");
+                }
+            } 
+        }
+
+        private string _strName;
+
+        public string Name
+        {
+            get { return _strName; }
+            set { 
+                if(value.Length > 3) { 
+                    _strName = value;
+                }
+                else { throw new Exception("Name shd have 3 chars atleast"); }
+            }
+        }
+
+        private DateTime _dtDOB;
+
+        public DateTime DOB
+        {
+            get { return _dtDOB; }
+            set { 
+                if (value.CompareTo(DateTime.Now) <= 0) { 
+                    _dtDOB = value; 
+                }
+                else { throw new Exception("DOB should be less than today"); }
+            }
+        }
+
+        private double _dbSal;
+
+        public double Salary
+        {
+            get { return _dbSal; }
+            set { _dbSal = value; }
+        }
+
+        // if there are no validations to be made:
+        //public double Salary { get; set; }
+
+        private static int EmpCounter;
+
+        public static int EmployeeCounter { 
+            get { return EmpCounter; } 
+            set { EmpCounter = value; } 
+        }
+
+        StringBuilder sb;
+        // string datatype is imm  utable hence use stringBuilder
+        public string Display()
+        {
+            sb =  new StringBuilder();
+            sb.Append("Emp Id " + EmployeeId + "\n");
+            sb.Append("Emp Name " + Name + "\n");
+            sb.Append("Emp DOB " + DOB.ToShortDateString() + "\n");
+            sb.Append("Emp Salary " + Salary + "\n");
+            return sb.ToString(); // convert char array to string
+        }
+        // Destructor (dispose) called whenever invoked
+        public void Dispose()
+        {
+            sb = null;
+            Console.WriteLine("Release all the resources like array, collection, db connections etc");
+        }
+
+        // Destructor/Finalize are in-deterministic they can be called without your knowledge
+        ~Employee()
+        {
+            Console.WriteLine("Destructors are in-deterministic in .net framework");
+        }
+
+    }
+}
---
+++ EmployeeObject/EmployeeObject/Program.cs
@@ -0,0 +1,54 @@
+﻿using EmployeeObject.MyObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1, emp2;
+
+            emp1 = new Employee();
+            emp2 = new Employee();
+            //emp2 = new Employee(20, "Test2");
+
+            emp1.EmployeeId = 10;
+            emp1.Name = "Test1";
+            emp1.DOB = new DateTime(2002, 2, 2);
+            emp1.Salary = 292.23;
+
+            emp2.EmployeeId = 20;
+            emp2.Name = "Test2";
+            emp2.DOB = Convert.ToDateTime("12/2/1999");
+            emp2.Salary = 292.23;
+
+            Console.WriteLine(emp1.EmployeeId + " " + emp2.EmployeeId);
+            Console.WriteLine(emp1.Name + " " + emp2.Name);
+
+            Console.WriteLine(emp1.Display());
+            Console.WriteLine(emp2.Display());
+
+
+            emp1.Dispose();
+            emp2.Dispose();
+
+            // assigning emp1/emp2 to NULL will not remove its reference to the EmployeeObject GC has to remove it
+            emp1 = null;
+            emp2 = null;     
+
+            Console.WriteLine("Number of employee objects is {0}", Employee.EmployeeCounter);
+
+            // Explicitly call GC - Destructor/Finalize invoked as GC finds emp1 & emp2 assigned to null
+            // GC deletes only those objects which are dead
+            System.GC.Collect();
+
+            Console.ReadLine();
+
+        }
+    } 
+}
